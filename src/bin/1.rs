7056e5758ca3545625dedf8e11a9dfd00b7b20b7
z4tz <z4tz87@gmail.com>
2024-12-01 10:04:15 +0100

    day 1 solution

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,11 +1,11 @@
 use aoc2024_rust::aoc_reader;
 use std::time::Instant;
+use num::abs;
 
 fn main() {
     let content = aoc_reader(1);
     let start = Instant::now(); // skip file IO in timing
-    let result1 = part1(&content);
-    let result2 = part2(&content);
+    let (result1, result2) = part1(&content);
     let duration = start.elapsed();
     println!(
         "Part 1 result: {result1}\n\
@@ -14,12 +14,26 @@ fn main() {
     println!("Execution took {:?}", duration)
 }
 
-fn part1(content: &str) -> i32 {
-    let _ = content.lines();
-    1
-}
+fn part1(content: &str) -> (i32, i32) {
+    let mut first: Vec<i32> = vec!();
+    let mut second: Vec<i32> = vec!();
 
-fn part2(content: &str) -> i32 {
-    let _ = content.lines();
-    2
+    for line in content.lines() {
+        let parts: Vec<i32> = line.split("   ").map(|x| x.parse().unwrap()).collect();
+        first.push(parts[0]);
+        second.push(parts[1]);
+    }
+
+    first.sort();
+    second.sort();
+
+    let diff: i32 = first.iter().zip(second.iter()).map(|(a, b)| abs(a - b)).sum();
+
+    //Part 2
+    let mut similarity_score = 0;
+    for number in first {
+        similarity_score += second.iter().filter(|&x| *x == number).count() as i32 * number;
+    }
+    
+    (diff, similarity_score)
 }
