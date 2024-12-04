36ea3cd8327b78b778430d03722be2d4fb6c8816
z4tz <z4tz87@gmail.com>
2024-12-04 22:43:15 +0100

    added boilerplate for remaining days

---
+++ src/days/day05.rs
@@ -0,0 +1,18 @@
+use crate::Solution;
+use std::time::{Duration, Instant};
+
+pub struct Day05 {}
+
+impl Solution for Day05 {
+    fn timed_solution(&self, data: &str) -> (i32, i32, Duration) {
+        let start = Instant::now(); // skip file IO in timing
+        let (result1, result2) = problem_name(data);
+        let duration = start.elapsed();
+        (result1, result2, duration)
+    }
+}
+
+fn problem_name(_data: &str) -> (i32, i32) {
+
+    (1,2)
+}
---
+++ src/days/day12.rs
@@ -0,0 +1,20 @@
+
+use crate::Solution;
+use std::time::{Duration, Instant};
+
+pub struct Day12 {}
+
+impl Solution for Day12 {
+    fn timed_solution(&self, data: &str) -> (i32, i32, Duration) {
+        let start = Instant::now(); // skip file IO in timing
+        let (result1, result2) = problem_name(data);
+        let duration = start.elapsed();
+        (result1, result2, duration)
+    }
+}
+
+fn problem_name(_data: &str) -> (i32, i32) {
+
+    (1,2)
+}
+
