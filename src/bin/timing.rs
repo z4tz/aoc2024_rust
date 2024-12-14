cd60c0caea2a74e3b563e2a33ebe12b87b51fbc2
z4tz <z4tz87@gmail.com>
2024-12-14 10:53:51 +0100

    Day 11

--- src/bin/timing.rs
+++ src/bin/timing.rs
@@ -6,7 +6,7 @@ use aoc2024_rust::{
 };
 
 fn main() {
-    let day = 9;
+    let day = 11;
     let runtime = 1;
     let data = aoc_reader(&day);
     let solution = get_solution(&day).expect("Unable to get solution");
--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,6 +1,6 @@
-
 use crate::Solution;
 use std::time::{Duration, Instant};
+use std::collections::HashMap;
 
 pub struct Day11 {}
 
@@ -13,8 +13,36 @@ impl Solution for Day11 {
     }
 }
 
-fn problem_name(_data: &str) -> (String, String) {
+fn problem_name(data: &str) -> (String, String) {
+    let mut stones:HashMap<i64, i64> = HashMap::new();
 
-    (String::new(), String::new())
+    for number in data.split_ascii_whitespace().map(|x| x.parse::<i64>().unwrap()) {
+        *stones.entry(number).or_insert(0) += 1;
+    }
+
+    let mut score1 = 0;
+    let mut score2 = 0;
+    for i in 0.. 75 {
+        let mut new_stones = HashMap::new();
+
+        for (value, count) in stones {
+            if value == 0 {
+                *new_stones.entry(1).or_insert(0) += count;
+            } else if value.to_string().len() % 2 == 0 {
+                let stonestring = value.to_string();
+                let stoneparts = stonestring.split_at(stonestring.len() / 2);
+                *new_stones.entry(stoneparts.0.parse::<i64>().unwrap()).or_insert(0) += count;
+                *new_stones.entry(stoneparts.1.parse::<i64>().unwrap()).or_insert(0) += count;
+            } else {
+                *new_stones.entry(value * 2024).or_insert(0) += count;
+            }
+        }
+        stones = new_stones;
+        if i == 24 { score1 = stones.values().sum::<i64>()};
+        if i == 74 { score2 = stones.values().sum::<i64>()};
+    }
+
+    (score1.to_string(), score2.to_string())
 }
+
 
