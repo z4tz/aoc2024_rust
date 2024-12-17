8e506a24169bf381107a31a19b135ad9a510b6a6
z4tz <z4tz87@gmail.com>
2024-12-17 17:35:16 +0100

    Day 14 wip

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,6 +1,8 @@
 
 use crate::Solution;
 use std::time::{Duration, Instant};
+use itertools::{Itertools};
+use regex::Regex;
 
 pub struct Day14 {}
 
@@ -13,7 +15,48 @@ impl Solution for Day14 {
     }
 }
 
-fn problem_name(_data: &str) -> (String, String) {
+#[derive(Debug)]
+struct Robot {
+    position: (i32, i32),
+    velocity: (i32, i32)
+}
+impl Robot {
+    fn new(x: i32, y: i32, vx: i32, vy: i32) -> Robot {
+        Robot { position:(x,y), velocity:(vx,vy) }
+    }
+    fn step(&self, steps: i32, dimensions: (i32, i32)) -> (i32, i32) {
+        let mut position = ((self.position.0 + self.velocity.0 * steps)%dimensions.0,
+        (self.position.1 + self.velocity.1 * steps)%dimensions.1);
+        if position.0 < 0 {
+            position.0 += dimensions.0;
+        }
+        if position.1 < 0 {
+            position.1 += dimensions.1;
+        }
+        position
+    }
+}
+
+
+fn problem_name(data: &str) -> (String, String) {
+    let re = Regex::new(r"p=([-\d]+),([-\d]+) v=([-\d]+),([-\d]+)").unwrap();
+    let dims = (11,7);
+    let time = 5;
+    let robots = re.captures_iter(data)
+        .map(|a|a.iter().skip(1)
+            .map(|a|a.unwrap().as_str().parse::<i32>().unwrap()).collect_vec())
+        .map(|a| Robot::new(a[0], a[1], a[2], a[3])).collect_vec();
+
+    let positions = robots.iter()
+        .map(|robot| robot.step(time, dims))
+        .collect_vec();
+    println!("{:?}", positions);
+
+
+
+    // println!("{:?}", robots);
+
+
 
     (String::new(), String::new())
 }
